4a9dbf5b13230a0385a1f77eb402f2289ba313d7
Beomul <qjagns67@gmail.com>
2026-08-27 11:54:35 +0900

    Initial commit

---
+++ src/main/java/com/example/discovery_config_project/DiscoveryConfigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.discovery_config_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryConfigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryConfigProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discovery_config_project/DiscoveryConfigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discovery_config_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryConfigProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
